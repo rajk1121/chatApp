133d09e5496c43bbcfcaa65b065bdd3fed0d7fce
rajk1121 <rajat.mishra1121@gmail.com>
2019-09-10 08:47:18 +0530

    v1.0

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bcrypt = require('bcrypt');
 const app = express();
 let rsocket;
 let rusr = "";
-const port = process.eventNames.PORT || 80;
+const port = process.env.PORT || 80;
 // var bodyParser = require('body-parser')
 
 
